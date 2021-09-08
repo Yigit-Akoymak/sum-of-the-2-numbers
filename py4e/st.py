831da0b28cb058d5ad88d7934352aea0f1043969
Yiğit Akoymak <90326062+Yigit-Akoymak@users.noreply.github.com>
2021-09-08 15:55:44 +0300

    Add files via upload

---
+++ py4e/st.py
@@ -0,0 +1,12 @@
+#Firs # QUESTION: Number 1
+print("please  enter a number")
+_first_Input =(int(input(":>")))
+if float(_first_Input) < float():
+    print("you have entered a negative Number")
+    quit()
+#Second # QUESTION: Number 2
+print("please enter 2.nd ")
+_Second_input=(int(input(":>")))
+#sum of the Number 1 and 2
+print("sum of the 2 of the numbers")
+print(_Second_input + _first_Input)
